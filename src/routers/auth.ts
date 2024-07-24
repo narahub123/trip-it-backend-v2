8dde952285cdf6deb18efc3f81fe67f6211d749e
nara <parknara0213@gmail.com>
2024-07-24 11:38:23 +0900

    240724 reissue 코드 수정

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -4,6 +4,6 @@ import express from "express";
 export default (router: express.Router) => {
   router.post("/join", join); // 회원 가입
   router.post("/login", login); // 로그인
-  router.get("/reissue", reissue); // refressh 토큰으로 access 토큰 재발급
+  router.get("/", reissue); // refressh 토큰으로 access 토큰 재발급
   router.post("/reissue", reissue); // refressh 토큰으로 access 토큰 재발급
 };
