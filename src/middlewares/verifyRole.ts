b31b7cf5e11b6a04aea70e246299ddde03e9584f
nara <parknara0213@gmail.com>
2024-07-30 16:46:15 +0900

    240730 등급에 따른 로그인 차단

---
+++ src/middlewares/verifyRole.ts
@@ -0,0 +1,31 @@
+import { getUserByEmail } from "../apis/users";
+import express from "express";
+
+export const verifyRole = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const { email } = req.body;
+
+  try {
+    const user = await getUserByEmail(email);
+    const role = user.role;
+
+    if (role === "ROLE_ADMIN" || role === "ROLE_USER") {
+      next();
+    } else if (role === "ROLE_A") {
+      return res.status(403).json({ code: 7, msg: "7일정지" });
+    } else if (role === "ROLE_B") {
+      return res.status(403).json({ code: 8, msg: "30일정지" });
+    } else if (role === "ROLE_C") {
+      return res.status(403).json({ code: 9, msg: "영구정지" });
+    } else if (role === "ROLE_D") {
+      return res.status(403).json({ code: 10, msg: "탈퇴회원" });
+    }
+  } catch (error) {
+    console.log(error);
+
+    return res.status(401).json({ code: 1, msg: "등급 확인 실패" });
+  }
+};
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,9 +1,10 @@
 import { checkToken, join, login, reissue } from "../controllers/auth";
 import express from "express";
+import { verifyRole } from "../middlewares/verifyRole";
 
 export default (router: express.Router) => {
   router.get("/", checkToken); // 토큰 여부 확인
   router.post("/join", join); // 회원 가입
-  router.post("/login", login); // 로그인
+  router.post("/login", verifyRole, login); // 로그인
   router.post("/reissue", reissue); // refressh 토큰으로 access 토큰 재발급
 };
