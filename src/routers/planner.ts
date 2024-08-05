887d647cd3b7786677643ad49e5779415ea01965
nara <parknara0213@gmail.com>
2024-08-05 15:56:55 +0900

    240805 일정 코드 수정

--- src/routers/planner.ts
+++ src/routers/planner.ts
@@ -9,7 +9,7 @@ export default (router: express.Router) => {
   router.get(`/home/apiList/:areaCode/:pageNo/:contentTypeId`, fetchPlaces); // 전체 내용 조회
   router.get(`/home/apiDetail/:contentId/`, fetchPlace);
   router.get(
-    `/apiSearch/:metroId/pageNo/contentTypeId/keyword`,
+    `/apiSearch/:metroId/:pageNo/:contentTypeId/:keyword`,
     fetchPlacesByKeyword
   );
 };
